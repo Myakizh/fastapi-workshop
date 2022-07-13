f0d4268b27d2202331867b8245be7fbb584b3b5a
Myakizh <xoxlovvalentin5@gmail.com>
2022-07-14 00:11:43 +0300

    place logic into services

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,22 +1,16 @@
 from typing import List
 from fastapi import APIRouter
 from fastapi import Depends
-from sqlalchemy.orm import Session
 
-import tables
 from database import get_session
 from models.operations import Operation
+from services.opetations import OperationsService
 
 router = APIRouter(
     prefix='/operations',
 )
 
 @router.get('/', response_model=List[Operation])
-def get_operations(session: Session = Depends(get_session)):
-    operations = (
-        session
-        .query(tables.Operation)
-        .all()
-    )
-    return operations
+def get_operations(service: OperationsService = Depends()):
+    return service.get_list()
     
