8f5a1862dae675f725250667d50a78764c13e80d
Myakizh <xoxlovvalentin5@gmail.com>
2022-07-14 01:09:16 +0300

    add filter by kind

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,9 +1,12 @@
-from typing import List
+from typing import (
+    List,
+    Optional,
+)
 from fastapi import APIRouter
 from fastapi import Depends
 
 from database import get_session
-from models.operations import Operation
+from models.operations import Operation, OperationKind
 from services.opetations import OperationsService
 
 router = APIRouter(
@@ -11,6 +14,9 @@ router = APIRouter(
 )
 
 @router.get('/', response_model=List[Operation])
-def get_operations(service: OperationsService = Depends()):
-    return service.get_list()
+def get_operations(
+    kind: Optional[OperationKind] = None,
+    service: OperationsService = Depends()
+):
+    return service.get_list(kind=kind)
     
