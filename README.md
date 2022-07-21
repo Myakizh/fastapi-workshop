4aa184725da1f6d0929066ae9e67bd503d2c1c2d
Myakizh <xoxlovvalentin5@gmail.com>
2022-07-21 17:10:55 +0300

    add registration and authentication

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Workshop
 
-https://www.youtube.com/watch?v=W6aqCFJp7Xo&ab_channel=Antidasoftware
+я сделать регистрация, бьіло много сложньіх ошибок, но они сами поуходили на следушие дни
+https://www.youtube.com/watch?v=lfT6a9VqyLM&t=3047s&ab_channel=Antidasoftware
--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -7,6 +7,8 @@ from fastapi import APIRouter, Depends, Response, status
 from database import get_session
 from models.operations import Operation, OperationCreate, OperationKind, OperationUpdate
 from services.opetations import OperationsService
+from models.auth import User
+from services.auth import get_curretn_user
 
 router = APIRouter(
     prefix='/operations',
@@ -15,39 +17,45 @@ router = APIRouter(
 @router.get('/', response_model=List[Operation])
 def get_operations(
     kind: Optional[OperationKind] = None,
+    user: User = Depends(get_curretn_user),
     service: OperationsService = Depends()
 ):
-    return service.get_list(kind=kind)
+    return service.get_list(user_id=user.id, kind=kind)
     
 @router.post('/', response_model=Operation)
 def create(
     operation_data: OperationCreate,
+    user: User = Depends(get_curretn_user),
     service: OperationsService = Depends()
 ):
-    return service.create(operation_data)
+    return service.create(user_id=user.id, operation_data=operation_data)
 
 @router.get('/{operation_id}', response_model=Operation)
 def get_operation(
     operation_id: int,
+    user: User = Depends(get_curretn_user),
     service: OperationsService = Depends()
 ):
-    return service.get(operation_id)
+    return service.get(user_id=user.id, operation_id=operation_id)
 
 @router.put('/{operation_id}', response_model=Operation)
 def updete_operation(
     operation_id: int,
     operation_data: OperationUpdate,
+    user: User = Depends(get_curretn_user),
     service: OperationsService = Depends()
 ):
     return service.update(
-        operation_id,
-        operation_data
+        user_id=user.id, 
+        operation_id=operation_id,
+        operation_data=operation_data
     )
 
 @router.delete('/{operation_id}')
 def delete_operation(
     operation_id: int,
+    user: User = Depends(get_curretn_user),
     service: OperationsService = Depends()
 ):
-    service.delete(operation_id)
+    service.delete(user_id=user.id, operation_id=operation_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
